56d72d79798c28fc660b23e105f043c4101ea60e
Alice Freda <alicefreda@gmail.com>
2019-10-14 20:56:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 2
-    return array.join(" and ")
-  elsif array.length < 2
+  if array.length < 2
     return array.join
+  elsif array.length == 2
+    return array.join(" and ")
   elsif array.length >= 3
     last_item = array.pop
     array << "and #{last_item}"

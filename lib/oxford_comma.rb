4400440e340be93d07cdbae6713c14d83dbbbb4c
Alice Freda <alicefreda@gmail.com>
2019-10-14 20:32:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length < 2
     return array.join
-  elsif array.length <= 3
+  elsif array.length >= 3
     array[-2] << ("and")
     return array.join (", ")
   end
